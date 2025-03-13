e4f53bb80daeae928a739fed33b6d2a22202a2ec
Cielo Maria Vega Godoy <a01741542@tec.mx>
2025-03-13 15:02:29 -0600

    movimiento ajolote

--- OxxoGame/Assets/Scripts/AjolotediaControl.cs
+++ OxxoGame/Assets/Scripts/AjolotediaControl.cs
@@ -4,24 +4,29 @@ public class AjolotediaControl : MonoBehaviour
 {
     public float velocidad;
     Animator animatorController;
-    public SpriteRenderer sr;
+    private Vector3 direccion = Vector3.left;
+    bool isup=false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        animatorController = GetComponent<Animator>();  
+        animatorController = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(collision.gameObject.CompareTag("escenario")){
-            velocidad *= -1;
+    {   
+        if(!isup){
+            direccion = Vector3.up;
+            isup=true;
+        }else{
+            direccion = Vector3.right;
+            isup=!isup;
         }
     }
     void Update()
     {
         //Mover al ajolote
-        this.transform.position += Vector3.left * Time.deltaTime * velocidad;
+        this.transform.position += direccion * Time.deltaTime * velocidad;
 
         //Eliminar al ajolote
         if(transform.position.y<=-50){
