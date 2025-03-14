e7e1aab3199651ae391c2eee421aa65e7da3a883
Regina <a00840840@tec.mx>
2025-03-14 03:34:53 -0600

    Ajolote Camina
    hooray

---
+++ OxxoGame/Assets/Scripts/Login & Menu/MenuAxolotlControl.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MenuAxolotlControl : MonoBehaviour
+{
+    // Adjust movement speed and jump force
+    public float moveSpeed;
+
+    // References to player components
+    public Rigidbody2D rig; // Controls the character's physics
+    public SpriteRenderer sr; // Manages the sprite visualization
+    Animator animatorController; // Animation controller
+
+    void Start()
+    {
+        // Gets the Animator component from the object
+        animatorController = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+
+        // Change the character's facing direction based on its X velocity
+        if(rig.linearVelocity.x > 0){
+            sr.flipX = false;
+        }else if(rig.linearVelocity.x < 0){
+            sr.flipX = true;
+        }
+    }
+
+
+    private void FixedUpdate()
+    {
+        // Capture player input on the horizontal axis (left/right arrows)
+        float xInput = Input.GetAxis("Horizontal");
+
+        // Update the character's Rigidbody2D velocity on the X-axis
+        rig.linearVelocity = new Vector2(xInput * moveSpeed, rig.linearVelocity.y);
+
+        // If the character is moving and not in the air, change animation to "walk"
+        if(xInput != 0 && rig.linearVelocity.y == 0){
+            UpdateAnimation(PlayerAnimation.walk);
+        }
+        else{
+            // If the character is not moving, change animation to "idle"
+            UpdateAnimation(PlayerAnimation.idle);
+        }
+    }
+
+    // Enumeration defining the different animation states of the player
+    public enum PlayerAnimation{
+        idle, walk, jump
+    }
+
+    // Method to update the character's animation based on its state
+    void UpdateAnimation(PlayerAnimation nameAnimation){
+        switch(nameAnimation){
+            case PlayerAnimation.idle:
+                // Set Animator parameters for "idle" state
+                animatorController.SetBool("isWalking", false);
+                break;
+
+            case PlayerAnimation.walk:
+                // Set Animator parameters for "walk" state
+                animatorController.SetBool("isWalking", true);
+                break;
+
+            case PlayerAnimation.jump:
+                // Set Animator parameters for "jump" state
+                animatorController.SetBool("isWalking", false);
+                break;
+        }
+    }
+}
