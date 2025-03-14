572385f5e4efff0246bc4049cc09e69c661f9102
Cielo Maria Vega Godoy <a01741542@tec.mx>
2025-03-13 18:14:17 -0600

    scriptity spripting

--- OxxoGame/Assets/Scripts/DiaControl.cs
+++ OxxoGame/Assets/Scripts/DiaControl.cs
@@ -3,15 +3,23 @@ using UnityEngine.SceneManagement;
 
 public class DiaControl : MonoBehaviour
 {
+    static public DiaControl Instance;
+    public UIControlDia uiController;
     void EndMiniGame(){
         SceneManager.LoadScene("MenuScene");
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Instance = this;
+        uiController.ShowCanva();
+        init();
     }
-
+    void init(){
+        if(uiController!=null){
+            uiController.StartTime();
+        }
+    }
     // Update is called once per frame
     void Update()
     {
