1d9e99798c3e5e9cb0236e3562f5021ed1cfdf8a
Adolfo <A01741767@tec.mx>
2025-04-09 19:06:19 -0600

    yipieeee

--- OxxoGame/Assets/Scripts/Login & Menu/MenuController.cs
+++ OxxoGame/Assets/Scripts/Login & Menu/MenuController.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class MenuController : MonoBehaviour
 {
     public GameObject[] Axolots;
-         Transform SpawnPoint;
+    public Transform SpawnPoint;
     private GameObject player;
 
     void Awake()
@@ -19,11 +19,11 @@ public class MenuController : MonoBehaviour
         GameObject playerSkin;
         int skinIndex = PlayerPrefs.HasKey("id_skin") ? PlayerPrefs.GetInt("id_skin") : 0;
         Debug.Log(skinIndex);
-        Debug.Log("Has key"+PlayerPrefs.HasKey("id_skin"));
-        Debug.Log("get int idskin"+PlayerPrefs.GetInt("id_skin"));
-        
+        Debug.Log("Has key" + PlayerPrefs.HasKey("id_skin"));
+        Debug.Log("get int idskin" + PlayerPrefs.GetInt("id_skin"));
+
         playerSkin = Axolots[skinIndex];
-        
+
 
         player = Instantiate(playerSkin, SpawnPoint.position, Quaternion.identity);
         Debug.Log(player);
